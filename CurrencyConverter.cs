bccd48f8f1cfe35b74c9d8ae69ae693f5a1707ce
BrunnoSM <brunno.miotto@gmail.com>
2023-09-14 13:33:49 -0300

    First Commit

---
+++ CurrencyConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ConvertibleCurrencies;
+
+class CurrencyConverter
+{
+
+    public double DollarExchangeRate { get; set; }
+    public double DollarPurchase { get; set; }
+
+    // Método Construtor
+    public CurrencyConverter(double dollarExchangeRate, double dollarPurchase)
+    {
+        DollarExchangeRate = dollarExchangeRate;
+        DollarPurchase = dollarPurchase;
+    }
+
+    public double CalculatesTax()
+    {
+        double iof = 6.0;
+        double tax = (DollarPurchase * iof) / 100;
+        return (DollarPurchase + tax) * DollarExchangeRate;
+    }
+
+    public override string ToString()
+    {
+        return $"Valor a ser pago em reais = {CalculatesTax().ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using ConvertibleCurrencies;
+
+namespace ProgramMain;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Qual é a cotação do dólar? ");
+        double dollarRate = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.Write("Quantos dólares você vai comprar? ");
+        double buyingDollar = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        CurrencyConverter price = new CurrencyConverter(dollarRate, buyingDollar);
+
+        Console.WriteLine();
+        Console.WriteLine(price);
+    }
+}
